e4f13647a993e7fc929d911e7cb86ea20dacbb5b
yuHuang01 <huang.y0112@gmail.com>
2021-03-27 12:05:15 +0100

    Added a tag in index.pug to navigate to the new form page, then declared the node version in package.json file to make deployment on Heroku easier

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages});
 });
 
+router.post('/new', (req, res) => {
+  messages.push({
+    "text": req.body.messageText,
+    "user": req.body.name,
+    "added": new Date()
+  });
+
+  res.redirect('/');
+})
+
 module.exports = router;
